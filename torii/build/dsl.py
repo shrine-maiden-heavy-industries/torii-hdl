00874afc2d7c84270d58d62f09659b1e51aa489d
Aki Van Ness <aki@lethalbit.net>
2022-11-12 23:48:23 -0500

    torii.build: some refactoring and typing of dsl.py

--- torii/build/dsl.py
+++ torii/build/dsl.py
@@ -1,6 +1,10 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
 from collections import OrderedDict
+from typing      import (
+	Literal, Optional, Tuple, Union, Iterator,
+	List, Dict, Callable
+)
 
 __all__ = (
 	'Pins',
@@ -15,7 +19,10 @@ __all__ = (
 )
 
 class Pins:
-	def __init__(self, names, *, dir = 'io', invert = False, conn = None, assert_width = None):
+	def __init__(
+		self, names : str, *, dir : Literal['i', 'o', 'io', 'oe'] = 'io', invert : bool = False,
+		conn : Optional[Tuple[str, Union[int, str]]] = None, assert_width : Optional[int] = None
+	) -> None:
 		if not isinstance(names, str):
 			raise TypeError(f'Names must be a whitespace-separated string, not {names!r}')
 		names = names.split()
@@ -23,9 +30,11 @@ class Pins:
 		if conn is not None:
 			conn_name, conn_number = conn
 			if not (isinstance(conn_name, str) and isinstance(conn_number, (int, str))):
-				raise TypeError('Connector must be None or a pair of string (connector name) and '
-								f'integer/string (connector number), not {conn!r}')
-			names = [f'{conn_name}_{conn_number}:{name}' for name in names]
+				raise TypeError(
+					'Connector must be None or a pair of string (connector name) and '
+					f'integer/string (connector number), not {conn!r}'
+				)
+			names = [ f'{conn_name}_{conn_number}:{name}' for name in names ]
 
 		if dir not in ('i', 'o', 'io', 'oe'):
 			raise TypeError(f'Direction must be one of "i", "o", "oe", or "io", not {dir!r}')
@@ -37,13 +46,13 @@ class Pins:
 		self.dir    = dir
 		self.invert = bool(invert)
 
-	def __len__(self):
+	def __len__(self) -> int:
 		return len(self.names)
 
-	def __iter__(self):
+	def __iter__(self) -> Iterator[str]:
 		return iter(self.names)
 
-	def map_names(self, mapping, resource):
+	def map_names(self, mapping : Dict[str, str], resource) -> List[str]:
 		mapped_names = []
 		for name in self.names:
 			while ":" in name:
@@ -53,16 +62,19 @@ class Pins:
 			mapped_names.append(name)
 		return mapped_names
 
-	def __repr__(self):
+	def __repr__(self) -> str:
 		return f'(pins{"-n" if self.invert else ""} {self.dir} {" ".join(self.names)})'
 
 
-def PinsN(*args, **kwargs):
+def PinsN(*args, **kwargs) -> Pins:
 	return Pins(*args, invert = True, **kwargs)
 
 
 class DiffPairs:
-	def __init__(self, p, n, *, dir = 'io', invert = False, conn = None, assert_width = None):
+	def __init__(
+		self, p : str, n : str, *, dir : Literal['i', 'o', 'io', 'oe'] = 'io', invert : bool = False,
+		conn : Optional[Tuple[str, Union[int, str]]] = None, assert_width : Optional[bool] = None
+	) -> None:
 		self.p = Pins(p, dir = dir, conn = conn, assert_width = assert_width)
 		self.n = Pins(n, dir = dir, conn = conn, assert_width = assert_width)
 
@@ -73,29 +85,29 @@ class DiffPairs:
 		self.dir    = dir
 		self.invert = bool(invert)
 
-	def __len__(self):
+	def __len__(self) -> int:
 		return len(self.p.names)
 
-	def __iter__(self):
+	def __iter__(self) -> Iterator[Tuple[str, str]]:
 		return zip(self.p.names, self.n.names)
 
-	def __repr__(self):
+	def __repr__(self) -> str:
 		return f'(diffpairs{"-n" if self.invert else ""} {self.dir} (p {" ".join(self.p.names)}) (n {" ".join(self.n.names)}))'
 
 
-def DiffPairsN(*args, **kwargs):
+def DiffPairsN(*args, **kwargs) -> DiffPairs:
 	return DiffPairs(*args, invert = True, **kwargs)
 
 
 class Attrs(OrderedDict):
-	def __init__(self, **attrs):
+	def __init__(self, **attrs : Dict[str, Union[int, str, Callable]]) -> None:
 		for key, value in attrs.items():
 			if not (value is None or isinstance(value, (str, int)) or hasattr(value, '__call__')):
 				raise TypeError(f'Value of attribute {key} must be None, int, str, or callable, not {value!r}')
 
 		super().__init__(**attrs)
 
-	def __repr__(self):
+	def __repr__(self) -> str:
 		items = []
 		for key, value in self.items():
 			if value is None:
@@ -106,22 +118,24 @@ class Attrs(OrderedDict):
 
 
 class Clock:
-	def __init__(self, frequency):
+	def __init__(self, frequency : Union[float, int]) -> None:
 		if not isinstance(frequency, (float, int)):
 			raise TypeError('Clock frequency must be a number')
 
 		self.frequency = float(frequency)
 
 	@property
-	def period(self):
+	def period(self) -> float:
 		return 1 / self.frequency
 
-	def __repr__(self):
+	def __repr__(self) -> str:
 		return f'(clock {self.frequency})'
 
 
 class Subsignal:
-	def __init__(self, name, *args):
+	def __init__(
+		self, name : str, *args : Union[Pins, DiffPairs, 'Subsignal', Attrs, Clock]
+	) -> None:
 		self.name  = name
 		self.ios   = []
 		self.attrs = Attrs()
@@ -134,15 +148,19 @@ class Subsignal:
 				if not self.ios:
 					self.ios.append(arg)
 				else:
-					raise TypeError('Pins and DiffPairs are incompatible with other location or '
-									f'subsignal constraints, but {arg!r} appears after {self.ios[-1]!r}')
+					raise TypeError(
+						'Pins and DiffPairs are incompatible with other location or '
+						f'subsignal constraints, but {arg!r} appears after {self.ios[-1]!r}'
+					)
 
 			elif isinstance(arg, Subsignal):
 				if not self.ios or isinstance(self.ios[-1], Subsignal):
 					self.ios.append(arg)
 				else:
-					raise TypeError('Subsignal is incompatible with location constraints, but '
-									f'{arg!r} appears after {self.ios[-1]!r}')
+					raise TypeError(
+						'Subsignal is incompatible with location constraints, but '
+						f'{arg!r} appears after {self.ios[-1]!r}'
+					)
 			elif isinstance(arg, Attrs):
 				self.attrs.update(arg)
 			elif isinstance(arg, Clock):
@@ -156,7 +174,7 @@ class Subsignal:
 			else:
 				raise TypeError('Constraint must be one of Pins, DiffPairs, Subsignal, Attrs, or Clock, not {arg!r}')
 
-	def _content_repr(self):
+	def _content_repr(self) -> str:
 		parts = []
 		for io in self.ios:
 			parts.append(repr(io))
@@ -166,12 +184,15 @@ class Subsignal:
 			parts.append(repr(self.attrs))
 		return " ".join(parts)
 
-	def __repr__(self):
+	def __repr__(self) -> str:
 		return f'(subsignal {self.name} {self._content_repr()})'
 
 class Resource(Subsignal):
 	@classmethod
-	def family(cls, name_or_number, number = None, *, ios, default_name, name_suffix = ''):
+	def family(
+		cls, name_or_number : Union[str, int], number : Optional[int] = None, *,
+		ios : List[Union[Pins, DiffPairs, 'Subsignal', Attrs, Clock]], default_name : str, name_suffix : str = ''
+	) -> 'Resource':
 		# This constructor accepts two different forms:
 		#  1. Number-only form:
 		#       Resource.family(0, default_name = "name", ios = [ Pins("A0 A1") ])
@@ -189,18 +210,22 @@ class Resource(Subsignal):
 		else: # name_or_number is name
 			return cls(name_or_number + name_suffix, number, *ios)
 
-	def __init__(self, name, number, *args):
+	def __init__(
+		self, name : str, number : int, *args : Union[Pins, DiffPairs, 'Subsignal', Attrs, Clock]
+	) -> None:
 		if not isinstance(number, int):
 			raise TypeError(f'Resource number must be an integer, not {number!r}')
 
 		super().__init__(name, *args)
 		self.number = number
 
-	def __repr__(self):
+	def __repr__(self) -> str:
 		return f'(resource {self.name} {self.number} {self._content_repr()})'
 
 class Connector:
-	def __init__(self, name, number, io, *, conn = None):
+	def __init__(
+		self, name : str, number : int, io : Union[str, Dict[str, str]], *, conn : Optional[Tuple[str, Union[int, str]]] = None
+	) -> None:
 		self.name    = name
 		self.number  = number
 		mapping = OrderedDict()
@@ -214,7 +239,7 @@ class Connector:
 				mapping[conn_pin] = plat_pin
 
 		elif isinstance(io, str):
-			for conn_pin, plat_pin in enumerate(io.split(), start=1):
+			for conn_pin, plat_pin in enumerate(io.split(), start = 1):
 				if plat_pin == '-':
 					continue
 
@@ -232,12 +257,12 @@ class Connector:
 				mapping[conn_pin] = f'{conn_name}_{conn_number}:{plat_pin}'
 		self.mapping = mapping
 
-	def __repr__(self):
+	def __repr__(self) -> str:
 		return f'(connector {self.name} {self.number} {" ".join(f"{conn}=>{plat}" for conn, plat in self.mapping.items())})'
 
-	def __len__(self):
+	def __len__(self) -> int:
 		return len(self.mapping)
 
-	def __iter__(self):
+	def __iter__(self) -> Iterator[Tuple[str, str]]:
 		for conn_pin, plat_pin in self.mapping.items():
 			yield f'{self.name}_{self.number}:{conn_pin}', plat_pin
