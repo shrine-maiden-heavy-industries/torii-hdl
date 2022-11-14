dd6c2a89af6a3a5c8a20e442996becd13d4424f4
Aki Van Ness <aki@lethalbit.net>
2022-11-14 06:22:43 -0500

    docs: updated sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,37 +1,67 @@
+# SPDX-License-Identifier: BSD-2-Clause
 import os, sys
-sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath('.'))
 
 import torii
 
-project = "Torii-HDL"
+project = 'Torii-HDL'
 version = torii.__version__
-release = version.split("+")[0]
-copyright = "2022, Shrine Maiden Heavy Industries"
+release = version.split('+')[0]
+copyright = '2022, Shrine Maiden Heavy Industries'
+language  = 'en'
 
 extensions = [
-	"sphinx.ext.intersphinx",
-	"sphinx.ext.doctest",
-    "sphinx.ext.todo",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx_rtd_theme",
-    "sphinxcontrib.platformpicker",
+	'sphinx.ext.autodoc',
+	'sphinx.ext.doctest',
+	'sphinx.ext.githubpages',
+	'sphinx.ext.intersphinx',
+	'sphinx.ext.napoleon',
+	'sphinx.ext.todo',
+	'sphinxcontrib.platformpicker',
+	'myst_parser',
+	'sphinx_rtd_theme',
 ]
 
 with open(".gitignore") as f:
-    exclude_patterns = [line.strip() for line in f.readlines()]
+	exclude_patterns = [line.strip() for line in f.readlines()]
+
+source_suffix = {
+	'.rst': 'restructuredtext',
+	'.md': 'markdown',
+}
 
 root_doc = "cover"
 
-intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
+pygments_style         = 'monokai'
+autodoc_member_order   = 'bysource'
+graphviz_output_format = 'svg'
+todo_include_todos     = True
 
-todo_include_todos = True
+intersphinx_mapping = {
+	'python': ('https://docs.python.org/3', None)
+}
+
 
 napoleon_google_docstring = False
-napoleon_numpy_docstring = True
-napoleon_use_ivar = True
-napoleon_custom_sections = ["Platform overrides"]
+napoleon_numpy_docstring  = True
+napoleon_use_ivar         = True
+napoleon_custom_sections  = [
+	'Platform overrides'
+]
 
-html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
-html_css_files = ["custom.css"]
+html_baseurl     = 'https://shrine-maiden-heavy-industries.github.io/torii-hdl/'
+html_theme       = 'sphinx_rtd_theme'
+html_copy_source = False
+
+html_theme_options = {
+	'collapse_navigation' : False,
+	'style_external_links': True,
+}
+
+html_static_path = [
+	'_static'
+]
+
+html_css_files = [
+	'custom.css'
+]
