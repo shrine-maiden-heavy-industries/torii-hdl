9fe120a8ae81044db5558f62d694dfa514ee4aa0
Aki Van Ness <aki@lethalbit.net>
2023-02-10 09:13:27 -0500

    torii: fixed the nocov for the `__init__.py` `__version__` bit

--- torii/__init__.py
+++ torii/__init__.py
@@ -2,11 +2,11 @@
 try:
 	from importlib import metadata
 	__version__ = metadata.version(__package__)
-except ImportError:
+except ImportError: # pragma: no cover
 	# No importlib_metadata. This shouldn't normally happen, but some people prefer not installing
 	# packages via pip at all, instead using PYTHONPATH directly or copying the package files into
 	# `lib/pythonX.Y/site-packages`. Although not a recommended way, we still try to support it.
-	__version__ = 'unknown' # :nocov:
+	__version__ = 'unknown'
 
 
 from .hdl import *
