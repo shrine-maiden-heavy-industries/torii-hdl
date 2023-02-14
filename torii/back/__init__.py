90aa895f1fa27db68e2aa3719455ae6736303cde
Aki Van Ness <aki@lethalbit.net>
2023-02-13 21:21:31 -0500

    torii.back: exported the cxxrtl, rtlil, and verilog `convert` and `convert_fragment` methods from the root of `torii.back`

--- torii/back/__init__.py
+++ torii/back/__init__.py
@@ -1,5 +1,21 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
-__all__ = (
+from .cxxrtl  import convert          as cxxrtl_convert
+from .cxxrtl  import convert_fragment as cxxrtl_convert_fragment
 
+from .rtlil   import convert          as rtlil_convert
+from .rtlil   import convert_fragment as rtlil_convert_fragment
+
+from .verilog import convert          as verilog_convert
+from .verilog import convert_fragment as verilog_convert_fragment
+
+__all__ = (
+	'cxxrtl_convert',
+	'cxxrtl_convert_fragment',
+
+	'rtlil_convert',
+	'rtlil_convert_fragment',
+
+	'verilog_convert',
+	'verilog_convert_fragment',
 )
