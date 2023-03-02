b9d2417a936f278347e6160b71d1de75ee2cbb14
Aki Van Ness <aki@lethalbit.net>
2023-03-02 14:26:14 -0500

    torii: Some more descriptive comments for some things

--- torii/tools/__init__.py
+++ torii/tools/__init__.py
@@ -24,12 +24,61 @@ class ToolNotFound(Exception):
 	pass
 
 def _get_tool(name: str) -> str:
+	'''
+	Get the tool name or path if overridden by an environment variable.
+
+	Parameters
+	----------
+	name : str
+		The tool/executable name.
+
+	Returns
+	-------
+	str
+		The tool/executable name if not found in the environment.
+
+	'''
+
 	return environ.get(tool_env_var(name), name)
 
 def has_tool(name: str) -> bool:
+	'''
+	Check to see if the tool or executable is in the current PATH.
+
+	Parameters
+	----------
+	name : str
+		The tool/executable name.
+
+	Returns
+	-------
+	bool
+		If the tool/executable was found in the current system PATH.
+
+	'''
+
 	return which(_get_tool(name)) is not None
 
 def require_tool(name: str) -> str:
+	'''
+	Return the fully resolved path to the requested tool or executable
+	if found, otherwise throw a ToolNotFound exception.
+
+	Parameters
+	----------
+	name : str
+		The tool/executable name.
+
+	Returns
+	-------
+	Path
+		The fully qualified path to the executable / tool.
+
+	Raises
+	------
+	ToolNotFound
+
+	'''
 	env_var = tool_env_var(name)
 	path = _get_tool(name)
 	if which(path) is None:
--- torii/util/string.py
+++ torii/util/string.py
@@ -32,6 +32,24 @@ def tcl_quote(string: str) -> str:
 	return '"' + sub(r'([$[\\"])', r'\\\1', string) + '"'
 
 def tool_env_var(name: str) -> str:
-	''' Convert the given tool name into a normalized version for environment variables '''
+	'''
+	Convert the given tool name into a normalized version for
+	environment variables.
+
+	This generally means replacing any `-`'s with `_`'s and any
+	`+`'s with `X`'s and converting the entire name to uppercase.
+
+	Parameters
+	----------
+	name : str
+		The tool/executable name.
+
+	Returns
+	-------
+	str
+		The name of the environment variable used to look for
+		for tool overrides.
+
+	'''
 
 	return name.upper().replace('-', '_').replace('+', 'X')
