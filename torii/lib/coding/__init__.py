6e3df19bea3f7176688f93d9dbf90e320f0c1fb9
Aki Van Ness <aki@lethalbit.net>
2023-03-31 13:15:49 -0400

    torii.lib.coding: Added shim for older imports from `torii.lib.coding` to prevent breaking older code for now

--- torii/lib/coding/__init__.py
+++ torii/lib/coding/__init__.py
@@ -1,1 +1,14 @@
 # SPDX-License-Identifier: BSD-2-Clause
+
+from .gray     import Encoder as GrayEncoder, Decoder as GrayDecoder
+from .one_hot  import Encoder, Decoder
+from .priority import Encoder as PriorityEncoder, Decoder as PriorityDecoder
+
+__all__ = (
+	'Encoder',
+	'Decoder',
+	'GrayEncoder',
+	'GrayDecoder',
+	'PriorityEncoder',
+	'PriorityDecoder',
+)
