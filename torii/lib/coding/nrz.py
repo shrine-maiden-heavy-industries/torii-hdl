c7b1e823509abe4fb3093bf5df15cc9540e77a43
Aki Van Ness <aki@lethalbit.net>
2023-06-10 11:50:05 -0400

    torii.lib.coding: Added stubs for NRZ-like codes

---
+++ torii/lib/coding/nrz.py
@@ -0,0 +1,97 @@
+# SPDX-License-Identifier: BSD-2-Clause
+
+from ...hdl import Elaboratable, Module
+
+
+__all__ = (
+
+)
+
+
+class NRZLEncoder(Elaboratable):
+	''' NRZL (Non-return-to-zero level) Encoder '''
+
+	def __init__(self) -> None:
+		pass
+
+	def elaborate(self, platform) -> Module:
+		m = Module()
+
+		return m
+
+class NRZLDecoder(Elaboratable):
+	''' NRZL (Non-return-to-zero level) Decoder '''
+
+	def __init__(self) -> None:
+		pass
+
+	def elaborate(self, platform) -> Module:
+		m = Module()
+
+		return m
+
+class NRZMEncoder(Elaboratable):
+	''' NRZM (Non-return-to-zero Mark) Encoder '''
+
+	def __init__(self) -> None:
+		pass
+
+	def elaborate(self, platform) -> Module:
+		m = Module()
+
+		return m
+
+class NRZMDecoder(Elaboratable):
+	''' NRZM (Non-return-to-zero Mark) Decoder '''
+
+	def __init__(self) -> None:
+		pass
+
+	def elaborate(self, platform) -> Module:
+		m = Module()
+
+		return m
+
+class NRZSEncoder(Elaboratable):
+	''' NRZS (Non-return-to-zero Space) Encoder '''
+
+	def __init__(self) -> None:
+		pass
+
+	def elaborate(self, platform) -> Module:
+		m = Module()
+
+		return m
+
+class NRZSDecoder(Elaboratable):
+	''' NRZS (Non-return-to-zero Space) Decoder '''
+
+	def __init__(self) -> None:
+		pass
+
+	def elaborate(self, platform) -> Module:
+		m = Module()
+
+		return m
+
+class NRZCEncoder(Elaboratable):
+	''' NRZC (Non-return-to-zero Change) Encoder '''
+
+	def __init__(self) -> None:
+		pass
+
+	def elaborate(self, platform) -> Module:
+		m = Module()
+
+		return m
+
+class NRZCDecoder(Elaboratable):
+	''' NRZC (Non-return-to-zero Change) Decoder '''
+
+	def __init__(self) -> None:
+		pass
+
+	def elaborate(self, platform) -> Module:
+		m = Module()
+
+		return m
