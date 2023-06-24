amaranth-lang/amaranth

test_sim: add failing test case for bitwise binary ops.
See https://github.com/amaranth-lang/amaranth/pull/826#event-9609577585.

tests/test_sim.py
@@ -927,3 +927,12 @@ class SimulatorRegressionTestCase(FHDLTestCase):
                 r"^Adding a clock process that drives a clock domain object named 'sync', "
                 r"which is distinct from an identically named domain in the simulated design$"):
             sim.add_clock(1e-6, domain=ClockDomain("sync"))
+
+    def test_bug_826(self):
+        sim = Simulator(Module())
+        def process():
+            self.assertEqual((yield C(0b0000, 4) | ~C(1, 1)), 0b0000)
+            self.assertEqual((yield C(0b1111, 4) & ~C(1, 1)), 0b0000)
+            self.assertEqual((yield C(0b1111, 4) ^ ~C(1, 1)), 0b1111)
+        sim.add_process(process)
+        sim.run()

shrine-maiden-heavy-industries/torii-hdl

tests/sim/test_sim.py
@@ -1382,3 +1382,14 @@ class SimulatorRegressionTestCase(ToriiTestSuiteCase):
 			r'which is distinct from an identically named domain in the simulated design$'
 		):
 			sim.add_clock(1e-6, domain = ClockDomain('sync'))
+
+	def test_bug_826(self):
+		sim = Simulator(Module())
+
+		def process():
+			self.assertEqual((yield Const(0b0000, 4) | ~Const(1, 1)), 0b0000)
+			self.assertEqual((yield Const(0b1111, 4) & ~Const(1, 1)), 0b0000)
+			self.assertEqual((yield Const(0b1111, 4) ^ ~Const(1, 1)), 0b1111)
+
+		sim.add_process(process)
+		sim.run()