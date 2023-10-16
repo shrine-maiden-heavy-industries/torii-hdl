51d5a9a3a5acb0d2adeb50733cc848218665d554
Aki Van Ness <aki@lethalbit.net>
2023-10-15 23:36:13 -0700

    torii: Added a deprecation  note in the `torii.asserts` module

--- torii/asserts.py
+++ torii/asserts.py
@@ -1,9 +1,17 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
+from warnings import warn
+
 from .hdl.ast import (
 	AnyConst, AnySeq, Assert, Assume, Cover, Fell, Initial,
 	Past, Rose, Stable
 )
+
+warn(
+	'The contents of `torii.asserts` have moved to `<FORMAL LIB MODULE>`',
+	DeprecationWarning, stacklevel = 2
+)
+
 
 __all__ = (
 	'AnyConst',
