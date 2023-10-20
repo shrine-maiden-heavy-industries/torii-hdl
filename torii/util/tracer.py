6aa95caf0d4bc43deb70acedaf3941836bffe838
Wanda <wanda@phinode.net>
2023-10-20 16:05:37 +0200

    tracer: recognize Python 3.13's CALL_KW opcode.
    Co-Authored-By: Wanda <wanda@phinode.net>
    Co-Authored-By: Aki Van Ness <aki@lethalbit.net>

--- torii/util/tracer.py
+++ torii/util/tracer.py
@@ -28,7 +28,9 @@ def get_var_name(depth: int = 2, default: Optional[Union[str, object]] = _raise_
 			call_index += 2
 		else:
 			break
-	if call_opc not in ('CALL_FUNCTION', 'CALL_FUNCTION_KW', 'CALL_FUNCTION_EX', 'CALL_METHOD', 'CALL'):
+	if call_opc not in (
+		'CALL_FUNCTION', 'CALL_FUNCTION_KW', 'CALL_FUNCTION_EX', 'CALL_METHOD', 'CALL', 'CALL_KW'
+	):
 		if default is _raise_exception:
 			raise NameNotFound
 		else:
