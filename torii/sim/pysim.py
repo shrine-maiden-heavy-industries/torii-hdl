000a13c09d609494efb00f968b091b2929d9f60a
Wanda <wanda@phinode.net>
2024-01-31 00:07:33 +0100

    sim: use `Value.cast` on traces.
    See kuznia-rdzeni/coreblocks#357.
    
    Co-Authored-By: Wanda <wanda@phinode.net>
    Co-Authored-By: Aki Van Ness <aki@lethalbit.net>

--- torii/sim/pysim.py
+++ torii/sim/pysim.py
@@ -8,7 +8,7 @@ from vcd        import VCDWriter
 from vcd.gtkw   import GTKWSave
 
 from ..hdl      import ClockSignal, ResetSignal
-from ..hdl.ast  import SignalDict
+from ..hdl.ast  import SignalDict, Value
 from ..hdl.ir   import Fragment
 from ._base     import BaseEngine, BaseSignalState, BaseSimulation
 from ._pyclock  import PyClockProcess
@@ -53,6 +53,7 @@ class _VCDWriter:
 		trace_names = SignalDict()
 		assigned_names = set()
 		for trace in traces:
+			trace = Value.cast(trace)
 			for trace_signal in trace._rhs_signals():
 				if trace_signal not in signal_names:
 					if trace_signal.name not in assigned_names:
