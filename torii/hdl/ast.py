f30d39fc01a91f10ff9590da69d956eb1072270f
Wanda <wanda@phinode.net>
2024-02-13 05:31:51 +0100

    hdl.ast: fix `shift_right` and `as_signed` edge cases.
    Co-Authored-By: Wanda <wanda@phinode.net>
    Co-Authored-By: Aki Van Ness <aki@lethalbit.net>

--- torii/hdl/ast.py
+++ torii/hdl/ast.py
@@ -468,7 +468,8 @@ class Value(metaclass = ABCMeta):
 			This ``Value`` reinterpreted as a signed integer.
 
 		'''
-
+		if len(self) == 0:
+			raise ValueError('Cannot create a 0-width signed value')
 		return Operator('s', [self])
 
 	def bool(self)  -> 'Operator':
@@ -702,6 +703,8 @@ class Value(metaclass = ABCMeta):
 		if amount < 0:
 			return self.shift_left(-amount)
 		if self.shape().signed:
+			if amount >= len(self):
+				amount = len(self) - 1
 			return self[amount:].as_signed()
 		else:
 			return self[amount:] # unsigned
