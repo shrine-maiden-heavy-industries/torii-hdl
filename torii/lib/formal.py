04f9aae8805ea550ce975bf097fe4946644bf8e8
Aki Van Ness <aki@lethalbit.net>
2024-03-14 02:22:10 -0700

    torii.lib: Added initial formal lib

--- torii/lib/formal.py
+++ torii/lib/formal.py
@@ -1,15 +1,8 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
-from warnings import warn
-
-from .lib.formal import (
+from ..hdl.ast import (
 	AnyConst, AnySeq, Assert, Assume, Cover, Fell, Initial,
 	Past, Rose, Stable
-)
-
-warn(
-	'The contents of `torii.asserts` have moved to `<FORMAL LIB MODULE>`',
-	DeprecationWarning, stacklevel = 2
 )
 
 __all__ = (
