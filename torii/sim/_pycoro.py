amaranth-lang/amaranth

sim: fix ValueCastable not being recognized as a coroutine command

amaranth/sim/_pycoro.py
@@ -1,7 +1,7 @@
 import inspect
 
 from ..hdl import *
-from ..hdl.ast import Statement, SignalSet
+from ..hdl.ast import Statement, SignalSet, ValueCastable
 from .core import Tick, Settle, Delay, Passive, Active
 from ._base import BaseProcess
 from ._pyrtl import _ValueCompiler, _RHSValueCompiler, _StatementCompiler
@@ -66,6 +66,8 @@ class PyCoroProcess(BaseProcess):
                     command = self.default_cmd
                 response = None
 
+                if isinstance(command, ValueCastable):
+                    command = Value.cast(command)
                 if isinstance(command, Value):
                     exec(_RHSValueCompiler.compile(self.state, command, mode="curr"),
                         self.exec_locals)

shrine-maiden-heavy-industries/torii-hdl

Co-Authored-By: Jaro Habiger <jarohabiger@googlemail.com>
Co-Authored-By: Aki Van Ness <aki@lethalbit.net>

torii/sim/_pycoro.py
@@ -3,7 +3,7 @@
 from inspect   import getfile, getlineno, iscoroutine, isgenerator
 
 from ..hdl     import ClockDomain, Const, Value
-from ..hdl.ast import SignalSet, Statement
+from ..hdl.ast import SignalSet, Statement, ValueCastable
 from ._base    import BaseProcess
 from ._pyrtl   import _RHSValueCompiler, _StatementCompiler, _ValueCompiler
 from .core     import Active, Delay, Passive, Settle, Tick
@@ -69,6 +69,8 @@ class PyCoroProcess(BaseProcess):
                     command = self.default_cmd
                 response = None
 
+                if isinstance(command, ValueCastable):
+                    command = Value.cast(command)
                 if isinstance(command, Value):
                     exec(_RHSValueCompiler.compile(self.state, command, mode = 'curr'),
                         self.exec_locals)