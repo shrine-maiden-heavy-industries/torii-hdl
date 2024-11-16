4448162c2550efc0415eb8db514b804e828d9cf4
Aki Van Ness <aki@lethalbit.net>
2024-11-15 16:33:38 -0800

    torii.hdl.ast: Fixed the nonsense in the `_ConstMeta.__call__` that mypy missed

--- torii/hdl/ast.py
+++ torii/hdl/ast.py
@@ -892,7 +892,7 @@ class Value(metaclass = ABCMeta):
 		pass # :nocov:
 
 class _ConstMeta(ABCMeta):
-	def __call__(cls, value: int, shape: ShapeCastable | None = None, src_loc_at: int = 0, **kwargs):
+	def __call__(cls, value: int, shape: 'Shape | int | range | type | ShapeCastable | None' = None, src_loc_at: int = 0, **kwargs):
 		if isinstance(shape, ShapeCastable):
 			return shape.const(value)
 		return super().__call__(value, shape, **kwargs, src_loc_at = src_loc_at + 1)
