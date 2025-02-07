7d1ae267bc72ee2200148598b2125e5431f3638a
Aki Van Ness <aki@lethalbit.net>
2025-02-07 12:19:24 -0800

    torii.lib.formal: Cleaned up imports

--- torii/lib/formal.py
+++ torii/lib/formal.py
@@ -1,9 +1,6 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
-from ..hdl.ast import (
-	AnyConst, AnySeq, Assert, Assume, Cover, Fell, Initial,
-	Past, Rose, Stable
-)
+from ..hdl.ast import AnyConst, AnySeq, Assert, Assume, Cover, Fell, Initial, Past, Rose, Stable
 
 __all__ = (
 	'AnyConst',
