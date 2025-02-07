1f722e93febd3f999f4d6b97ec9cb47e175ec8e0
Aki Van Ness <aki@lethalbit.net>
2025-02-07 12:22:06 -0800

    torii: Cleaned up imports

--- torii/__init__.py
+++ torii/__init__.py
@@ -2,10 +2,7 @@
 try:
 	from importlib import metadata
 	__version__ = metadata.version(__package__)
-except ImportError: # pragma: no cover
-	# No importlib_metadata. This shouldn't normally happen, but some people prefer not installing
-	# packages via pip at all, instead using PYTHONPATH directly or copying the package files into
-	# `lib/pythonX.Y/site-packages`. Although not a recommended way, we still try to support it.
+except ImportError: # :nocov:
 	__version__ = 'unknown'
 
 
@@ -35,7 +32,7 @@ __all__ = (
 	'ResetSignal',
 	'Shape',
 	'Signal',
-	'Value',
 	'signed',
 	'unsigned',
+	'Value',
 )
