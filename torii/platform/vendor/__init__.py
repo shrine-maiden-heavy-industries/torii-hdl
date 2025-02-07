bc89dd868924d3263dd7e169a554614c87b3a06c
Aki Van Ness <aki@lethalbit.net>
2025-02-07 12:11:13 -0800

    torii.platform.vendor: Cleaned up imports

--- torii/platform/vendor/__init__.py
+++ torii/platform/vendor/__init__.py
@@ -1,24 +1,18 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
-from .gowin      import GowinPlatform
 from .altera     import AlteraPlatform
-from .lattice    import (
-	LatticeECP5Platform, LatticeICE40Platform, LatticeMachXO2Platform, LatticeMachXO3LPlatform
-)
+from .gowin      import GowinPlatform
+from .lattice    import LatticeECP5Platform, LatticeICE40Platform, LatticeMachXO2Platform, LatticeMachXO3LPlatform
 from .quicklogic import QuicklogicPlatform
 from .xilinx     import XilinxPlatform
 
 __all__ = (
-	'GowinPlatform',
-
 	'AlteraPlatform',
-
+	'GowinPlatform',
 	'LatticeECP5Platform',
 	'LatticeICE40Platform',
 	'LatticeMachXO2Platform',
 	'LatticeMachXO3LPlatform',
-
 	'QuicklogicPlatform',
-
 	'XilinxPlatform',
 )
