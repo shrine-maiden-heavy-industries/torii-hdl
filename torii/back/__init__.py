04d963f65d4cab7b3ea249f62580c4f3a8886204
Aki Van Ness <aki@lethalbit.net>
2025-02-07 12:21:34 -0800

    torii.back: Cleaned up imports

--- torii/back/__init__.py
+++ torii/back/__init__.py
@@ -1,12 +1,10 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
-from .cxxrtl  import convert          as cxxrtl_convert
+from .cxxrtl  import convert as cxxrtl_convert
 from .cxxrtl  import convert_fragment as cxxrtl_convert_fragment
-
-from .rtlil   import convert          as rtlil_convert
+from .rtlil   import convert as rtlil_convert
 from .rtlil   import convert_fragment as rtlil_convert_fragment
-
-from .verilog import convert          as verilog_convert
+from .verilog import convert as verilog_convert
 from .verilog import convert_fragment as verilog_convert_fragment
 
 __all__ = (
