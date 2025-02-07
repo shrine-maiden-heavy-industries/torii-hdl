5c534bd0c4b504396fcb6b8b34c48cb53c9a1af2
Aki Van Ness <aki@lethalbit.net>
2025-02-07 12:16:21 -0800

    torii.lib.soc.csr.wishbone: Cleaned up imports

--- torii/lib/soc/csr/wishbone.py
+++ torii/lib/soc/csr/wishbone.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
-from ....           import Cat, Elaboratable, Module, Signal
+from ....hdl.ast    import Cat, Signal
+from ....hdl.dsl    import Module
+from ....hdl.ir     import Elaboratable
 from ....util.units import log2_exact
 from ..memory       import MemoryMap
 from ..wishbone     import Interface as WishboneInterface
