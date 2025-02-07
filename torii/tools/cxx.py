ecbd8f23b08f3815be726c06ff96012f970d8f0c
Aki Van Ness <aki@lethalbit.net>
2025-02-07 12:07:33 -0800

    torii.tools.cxx: Cleaned up imports

--- torii/tools/cxx.py
+++ torii/tools/cxx.py
@@ -1,9 +1,10 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
-from setuptools.command.build_ext import new_compiler
 from os                           import chdir, getcwd
 from sysconfig                    import get_config_var
 from tempfile                     import TemporaryDirectory
+
+from setuptools.command.build_ext import new_compiler
 
 __all__ = (
 	'build_cxx',
