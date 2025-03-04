eb20ecf2ce474cf220ba2aa9f884dfc8c9c58859
Aki Van Ness <aki@lethalbit.net>
2025-03-04 07:47:43 -0800

    torii: Added deprecation notifications for root module HDL imports for #29

--- torii/__init__.py
+++ torii/__init__.py
@@ -5,12 +5,8 @@ try:
 except ImportError: # :nocov:
 	__version__ = 'unknown'
 
-from .hdl import (
-	Array, Cat, ClockDomain, ClockSignal, Const, DomainRenamer,
-	Elaboratable, EnableInserter, Fragment, Instance, Memory,
-	Module, Mux, Record, ResetInserter, ResetSignal, Shape,
-	Signal, Value, signed, unsigned
-)
+from warnings  import warn
+from importlib import import_module
 
 __all__ = (
 	'Array',
@@ -35,3 +31,16 @@ __all__ = (
 	'unsigned',
 	'Value',
 )
+
+def __dir__() -> list[str]:
+	return list({*globals(), *__all__, '__version__'})
+
+def __getattr__(name: str):
+	if name in __all__:
+		warn(
+			'Importing HDL constructs from the root Torii module is deprecated, please import from `torii.hdl` instead',
+			DeprecationWarning
+		)
+		return import_module('.hdl', __name__).__dict__[name]
+	if name not in __dir__():
+		raise AttributeError(f'Module {__name__!r} has not attribute {name!r}')
