shrine-maiden-heavy-industries/torii-hdl

commit bce8a1eb80161f6a0e8843816c508684b464ffad

torii.hdl.dsl: Improved the typing for FSM and Switch handling

torii/hdl/dsl.py
@@ -4,7 +4,6 @@ import warnings
 from collections     import OrderedDict
 from collections.abc import Callable, Iterable, Generator
 from contextlib      import _GeneratorContextManager, contextmanager
-from enum            import Enum
 from functools       import wraps
 from sys             import version_info
 from typing          import ParamSpec, Any, TypedDict, TYPE_CHECKING
@@ -18,7 +17,7 @@ from .ast            import (
 from .cd             import ClockDomain
 from .ir             import Elaboratable, Fragment
 from .xfrm           import SampleDomainInjector
-from .._typing       import SrcLoc
+from .._typing       import SrcLoc, SwitchCaseT
 
 if TYPE_CHECKING:
 	from ..build.plat import Platform
@@ -170,9 +169,6 @@ class FSM:
 			self.encoding[name] = len(self.encoding)
 		return Operator('==', [ self.state, self.encoding[name] ], src_loc_at = 0)
 
-_Pattern = int | str | Enum
-_PatternTuple = tuple[_Pattern, ...]
-
 class _IfDict(TypedDict):
 	depth: int
 	tests: list
@@ -182,9 +178,9 @@ class _IfDict(TypedDict):
 
 class _SwitchDict(TypedDict):
 	test: Value
-	cases: OrderedDict[_PatternTuple, _StatementList]
+	cases: OrderedDict[SwitchCaseT, _StatementList]
 	src_loc: SrcLoc
-	case_src_locs: dict[_PatternTuple, SrcLoc]
+	case_src_locs: dict[SwitchCaseT, SrcLoc]
 
 class _FSMDict(TypedDict):
 	name: str
@@ -193,7 +189,7 @@ class _FSMDict(TypedDict):
 	domain: str
 	encoding: OrderedDict[str, int]
 	decoding: OrderedDict
-	states: OrderedDict[str, object]
+	states: OrderedDict[str, _StatementList]
 	src_loc: SrcLoc
 	state_src_locs: dict[str, SrcLoc]
 
@@ -568,8 +564,14 @@ class Module(_ModuleBuilderRoot, Elaboratable):
 			switch_test, switch_cases = data['test'], data['cases']
 			switch_case_src_locs = data['case_src_locs']
 
-			self._statements.append(Switch(switch_test, switch_cases,
-				src_loc = src_loc, case_src_locs = switch_case_src_locs))
+			self._statements.append(
+				Switch(
+					switch_test,
+					switch_cases,
+					src_loc = src_loc,
+					case_src_locs = switch_case_src_locs
+				)
+			)
 
 		if name == 'FSM':
 			if TYPE_CHECKING:

torii/hdl/ast.py
@@ -7,7 +7,7 @@ import operator
 from abc               import ABCMeta, abstractmethod
 from collections       import OrderedDict
 from collections.abc   import (
-	Iterable, Iterator, MutableMapping, MutableSequence, MutableSet, Sequence, Callable, Generator
+	Iterable, Iterator, Mapping, MutableMapping, MutableSequence, MutableSet, Sequence, Callable, Generator
 )
 from typing            import TYPE_CHECKING, TypeAlias, TypeVar, Literal, NoReturn, Generic, ParamSpec
 from types             import NotImplementedType
@@ -1997,7 +1997,7 @@ def Cover(test: ValueCastT, *, name: str | None = None, src_loc_at: int = 0) -> 
 
 # @final
 class Switch(Statement):
-	def __init__(self, test: ValueCastT, cases: dict[SwitchCaseT, Iterable[object] | object], *,
+	def __init__(self, test: ValueCastT, cases: Mapping[SwitchCaseT, Iterable[object] | _StatementList], *,
 		src_loc: SrcLoc | None = None, src_loc_at: int = 0, case_src_locs: dict[SwitchCaseT, SrcLoc] = {}
 	) -> None:
 		if src_loc is None:

commit 1733f487604ea6c14c3806c46fbef521b34129fb

torii.hdl.dsl: Fixed a bug that was introduced in bce8a1e where this one instance of `_PatternTuple` was missed

torii/hdl/dsl.py
@@ -355,7 +355,7 @@ class Module(_ModuleBuilderRoot, Elaboratable):
 			assert switch_data is None or isinstance(switch_data, _SwitchDict)
 		if switch_data is None:
 			raise SyntaxError('Case outside of Switch block')
-		new_patterns: _PatternTuple = ()
+		new_patterns: SwitchCaseT = ()
 		if () in switch_data['cases']:
 			warnings.warn(
 				'Case statements are order-dependant, any Case after a Default will be ignored',