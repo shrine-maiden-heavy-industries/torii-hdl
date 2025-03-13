25d26f3af5c918e9fd945e17eb34860877ac0712
Aki Van Ness <aki@lethalbit.net>
2025-03-12 20:14:48 -0700

    torii.lib.bus.amba: Stubbed out AMBA busses

---
+++ torii/lib/bus/amba/atb/__init__.py
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: BSD-2-Clause
+
+'''
+
+.. _AMBA5: https://developer.arm.com/Architectures/AMBA
+.. _IHI0032: https://developer.arm.com/documentation/ihi0032/latest
+
+ARM `AMBA5`_ Advanced Trace Bus (ATB)
+-------------------------------------
+
+For detailed information see ARM technical document `IHI0032`_.
+
+
+'''
+
+__all__ = (
+
+)
