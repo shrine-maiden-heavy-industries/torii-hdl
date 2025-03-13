2013c5e73e97e572537ea2eb580c24300afafd87
Aki Van Ness <aki@lethalbit.net>
2025-03-12 20:14:48 -0700

    torii.lib.bus.amba: Stubbed out AMBA busses

--- torii/lib/bus/__init__.py
+++ torii/lib/bus/__init__.py
@@ -3,9 +3,10 @@
 '''
 
 .. _Wishbone: https://opencores.org/howto/wishbone
+.. _AMBA5: https://developer.arm.com/Architectures/AMBA
 
 The :py:mod:`torii.lib.bus` module provides industry standard bus interfaces, crossbars, and other
-utilities for busses such as `Wishbone`_.
+utilities for busses such as `Wishbone`_ and `AMBA5`_ AXI.
 
 '''
 
