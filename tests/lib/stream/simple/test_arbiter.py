3d8938c675c512feca4cf7ad70cf57e8b105563d
Aki Van Ness <aki@lethalbit.net>
2025-04-02 18:43:01 -0700

    tests.lib.stream.simple: Split tests into their own module and split test cases into individual files

--- tests/lib/stream/simple/test_arbiter.py
+++ tests/lib/stream/simple/test_arbiter.py
@@ -8,33 +8,6 @@ from torii.lib.stream.simple import StreamArbiter, StreamInterface
 from torii.sim.core          import Settle
 from torii.test              import ToriiTestCase
 
-from ...utils                import ToriiTestSuiteCase
-
-class StreamInterfaceTestCase(ToriiTestSuiteCase):
-
-	def test_record(self) -> None:
-		s0 = StreamInterface()
-		self.assertRepr(s0, '(rec s0 data valid first last ready)')
-
-		s1 = StreamInterface(extra = [('meow', 8)])
-		self.assertRepr(s1, '(rec s1 data valid first last ready meow)')
-
-		s2 = StreamInterface(name = 'ULPI')
-		self.assertRepr(s2, '(rec ULPI data valid first last ready)')
-
-	def test_sizes(self) -> None:
-		s0 = StreamInterface()
-		self.assertEqual(s0.data.width,  8)
-		self.assertEqual(s0.valid.width, 1)
-
-		s1 = StreamInterface(data_width = 32, valid_width = None)
-		self.assertEqual(s1.data.width,  32)
-		self.assertEqual(s1.valid.width,  4)
-
-		s2 = StreamInterface(data_width = 16, valid_width = 8)
-		self.assertEqual(s2.data.width,  16)
-		self.assertEqual(s2.valid.width,  8)
-
 # TODO(aki): This test needs more proper assertions, the problem being is it's not super easy
 #            to introspect, plus the StreamArbiter internals are actually kinda simple, so
 #            this test is mostly just based on ✨ v i b e s ✨ and looking at the VCD :v
