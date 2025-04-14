19b39c935dd9510122c7577f9dfe41e091f884d7
dragonmux <git@dragonmux.network>
2025-04-14 21:10:27 +0100

    torii.lib.hash.crc.crc32: Created a high f(max), low area usage CRC32 implementation that runs bitwise

---
+++ tests/lib/hash/crc/test_crc32.py
@@ -0,0 +1,58 @@
+# SPDX-License-Identifier: BSD-2-Clause
+
+from zlib import crc32
+
+from torii.lib.hash.crc.crc32 import BitwiseCRC32
+from torii.sim                import Settle
+from torii.test               import ToriiTestCase
+
+from ....utils                import ToriiTestSuiteCase
+
+CRC_DATA = b"Hello binary world"
+
+class BitwiseCRC32TestCase(ToriiTestSuiteCase):
+	dut: BitwiseCRC32 = BitwiseCRC32
+	dut_args = {
+		# CRC-32 polynomial most commonly used (PNG, Gzip, PKZIP, NVMe, SATA, Ethernet, etc)
+		'polynomial': 0xedb88320
+	}
+
+	@ToriiTestCase.simulation
+	@ToriiTestCase.sync_domain(domain = 'sync')
+	def test_crc32(self):
+		# Check that the controller comes up into the proper state
+		self.assertEqual((yield self.dut.done), 0)
+		yield
+		self.assertEqual((yield self.dut.done), 1)
+		self.assertEqual((yield self.dut.crc), 0xffffffff)
+		yield
+		# Now ask it to CRC32 the string "Hello binary world"
+		for byte in CRC_DATA:
+			# Load the byte to CRC
+			yield self.dut.data.eq(byte)
+			yield self.dut.valid.eq(1)
+			yield
+			self.assertEqual((yield self.dut.done), 1)
+			yield self.dut.data.eq(0)
+			yield self.dut.valid.eq(0)
+			yield
+			self.assertEqual((yield self.dut.done), 0)
+			# Wait for the computation to complete
+			yield from self.wait_until_high(self.dut.done)
+
+		# Use the zlib module from the stdlib to compute a reference value
+		checkCRC32 = crc32(CRC_DATA, 0xffffffff)
+		# And check if the one computed matches
+		self.assertEqual((yield self.dut.crc), checkCRC32)
+		yield
+
+		# Finally, check if the reset signal works properly
+		yield self.dut.reset.eq(1)
+		yield
+		yield self.dut.reset.eq(0)
+		yield
+		self.assertEqual((yield self.dut.done), 0)
+		yield
+		self.assertEqual((yield self.dut.done), 1)
+		self.assertEqual((yield self.dut.crc), 0xffffffff)
+		yield
