cc82f74cc5fd7c6fe1cf0086c8bed7fef4883478
Aki Van Ness <aki@lethalbit.net>
2025-06-24 20:12:03 -0700

    torii.util: Added tests and comments for the rest of the bits of `torii.util`

---
+++ tests/util/test_misc.py
@@ -0,0 +1,68 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# torii: foo0=1, bar0=yes, baz0=enable
+# torii: foo1=0, bar0=no, baz0=disable
+# torii: foo2=0b1, bar2=0o2, baz2=0x3
+# torii: foo3=invalid, bar3=no, baz3=2
+
+from torii.test import ToriiTestCase
+from torii.util import flatten, get_linter_option, get_linter_options, union
+
+class MiscUtilsTestCase(ToriiTestCase):
+	def test_flatten(self):
+		self.assertEqual(list(flatten([])), [])
+		self.assertEqual(list(flatten([[], [], []])), [])
+		self.assertEqual(list(flatten([[[], [], []], []])), [])
+		self.assertEqual(list(flatten([[[[], []], []], []])), [])
+		self.assertEqual(list(flatten([[], [[], []], []])), [])
+		self.assertEqual(list(flatten([[], [[0], []], []])), [0])
+		self.assertEqual(list(flatten([[], [[], [], 0], []])), [0])
+		self.assertEqual(list(flatten([[], [[], []], [0]])), [0])
+		self.assertEqual(list(flatten([[], [[], []], [], 0])), [0])
+		self.assertEqual(list(flatten([0, 1, 2, 3])), [0, 1, 2, 3])
+		self.assertEqual(list(flatten([0, [1, 2], 3])), [0, 1, 2, 3])
+		self.assertEqual(list(flatten([0, [1, [2, 3]]])), [0, 1, 2, 3])
+		self.assertEqual(list(flatten([0, [1, 2], [3]])), [0, 1, 2, 3])
+		self.assertEqual(list(flatten([0, [1, 2], [[3]]])), [0, 1, 2, 3])
+		self.assertEqual(list(flatten([0, '1', object, 3.0])), [0, '1', object, 3.0])
+		self.assertEqual(list(flatten([0, ['1', object], 3.0])), [0, '1', object, 3.0])
+		self.assertEqual(list(flatten([0, ['1'], [object, 3.0]])), [0, '1', object, 3.0])
+		self.assertEqual(list(flatten([[0, ['1'], [object, 3.0]]])), [0, '1', object, 3.0])
+
+	def test_get_linter_option_invalid(self):
+		with self.assertRaisesRegex(
+			TypeError,
+			r'^Expected type to be either \'bool\' or \'int\', not <class \'float\'>$',
+		):
+			get_linter_option(__file__, 'foo0', float, 0.0) # type: ignore
+
+	def test_get_linter_option(self):
+		self.assertEqual(get_linter_option(__file__, 'foo0', bool, False), True)
+		self.assertEqual(get_linter_option(__file__, 'bar0', bool, False), True)
+		self.assertEqual(get_linter_option(__file__, 'baz0', bool, False), True)
+
+		# TODO(aki): Un-comment once we fix the `get_linter_options` call
+# 		self.assertEqual(get_linter_option(__file__, 'foo1', bool, True), False)
+# 		self.assertEqual(get_linter_option(__file__, 'bar1', bool, True), False)
+# 		self.assertEqual(get_linter_option(__file__, 'baz1', bool, True), False)
+#
+# 		self.assertEqual(get_linter_option(__file__, 'foo2', int, 0), 1)
+# 		self.assertEqual(get_linter_option(__file__, 'bar2', int, 0), 2)
+# 		self.assertEqual(get_linter_option(__file__, 'baz2', int, 0), 3)
+#
+# 		self.assertEqual(get_linter_option(__file__, 'foo3', int, 0), 0)
+# 		self.assertEqual(get_linter_option(__file__, 'bar3', bool, True), False)
+# 		self.assertEqual(get_linter_option(__file__, 'baz3', int, 0), 2)
+
+		self.assertEqual(get_linter_option(__file__, 'NotFound32', int, 0), 0)
+
+	def test_get_linter_options(self):
+		self.assertEqual(get_linter_options(__file__), {
+			'foo0': '1',
+			'bar0': 'yes',
+			'baz0': 'enable'
+		})
+
+	def test_union(self):
+		# TODO(aki): We should have a more "comprehensive" test, but it's pretty simple
+		self.assertEqual(union([0, 1, 2, 3, 4]), 7)
+		self.assertEqual(union([4, 6, 8], 2), 14)
--- torii/util/__init__.py
+++ torii/util/__init__.py
@@ -1,9 +1,11 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
 from collections.abc import Generator, Iterable
+from functools       import cache
 from linecache       import getlines
 from re              import Match, compile
 from typing          import TypeAlias, TypeVar
+from operator        import ior
 
 __all__ = (
 	'flatten',
@@ -14,30 +16,93 @@ __all__ = (
 
 T = TypeVar('T')
 
+# NOTE(aki): The explicit `str` in the type is to make type checking happy
 def flatten(iter: Iterable[str | T | Iterable]) -> Generator[str | T]:
+	'''
+	Flatten nested iterables into a single iterator.
+
+	Parameters
+	----------
+	iter : Iterable[str | T | Iterable]
+		The nested iterator to flatten
+
+	Returns
+	-------
+	Generator[str | T]
+		The flattened sequence.
+	'''
+
 	for element in iter:
 		if isinstance(element, str) or not isinstance(element, Iterable):
 			yield element
 		else:
 			yield from flatten(element)
 
-def union(i: Iterable[T], start: T | None = None) -> T:
-	# NOTE(aki): The two ignores below are due to mypy having a hard time seeing through the loop/if-else
-	r = start
-	for e in i:
-		if r is None:
-			r = e
-		else:
-			r |= e # type: ignore
-	return r # type: ignore
 
+def union(iter: Iterable[T], start: T | None = None) -> T:
+	'''
+	Apply :py:meth:`operator.ior` over the iterable ``iter``.
+
+	Unlike :py:meth:`functools.accumulate`, this method only returns the final result, and not
+	all the intermediate steps.
+
+	Parameters
+	----------
+	iter : Iterable[T]
+		The iterable to apply :py:meth:`operator.ior` over.
+
+	start : T | None
+		The initial value for the result of the union.
+
+	Returns
+	-------
+	T
+		The result of the union operation.
+
+	'''
+
+	# NOTE(aki): The two ignores below are due `operator.ior` being stupid and using `Any` for typing rather than a typevar
+	result = start
+	for element in iter:
+		if result is None:
+			result = element
+		else:
+			result = ior(result, element) # type: ignore
+	return result # type: ignore
+
+@cache
 def get_linter_options(filename: str) -> dict[str, str]:
-	magic_comment = compile(r'^#\s*torii:\s*((?:\w+=\w+\s*)(?:,\s*\w+=\w+\s*)*)\n$')
+	'''
+	Read the given Python source file given by ``filename`` and extract any "Magic Comments" that Torii knows how to parse
+	and return them as a key-value set of options.
+
+	The "Magic Comments" look as follows:
+
+	.. code-block:: python
+
+		# torii: NAME=VALUE
+		# torii: NAME=VALUE, NAME2=VALUE2
+
+	The general format is the string ``# torii:`` followed by one or more ``NAME=VALUE`` pairs separated by commas. ``NAME`` and
+	``VALUE`` are one or more alphanumeric characters.
+
+	Parameters
+	----------
+	filename : str
+		The name of the Python source file to ingest
+
+	Returns
+	-------
+	dict[str, str]
+		All of the found settings from the given file.
+	'''
+
+	magic_comment = compile(r'^#\s*torii:\s*((?:\w+=\w+\s*)(?:,\s*\w+=\w+\s*)*)$')
 
 	# Check the first five lines of the file, because it might not be first
 	lines = getlines(filename)[0:5]
 	if len(lines) > 0:
-		# NOTE(aki): using the lambda in `filter` is aa mypy blind-spot, ignore this
+		# NOTE(aki): using the lambda in `filter` is a mypy blind-spot, ignore this
 		matches: list[Match] = list(filter(lambda m: m is not None, map(magic_comment.match, lines))) # type: ignore
 
 		if len(matches) > 0:
@@ -46,11 +111,52 @@ def get_linter_options(filename: str) -> dict[str, str]:
 
 LinterOptionType: TypeAlias = bool | int
 
+@cache
 def get_linter_option(
 	filename: str , name: str, type: type[LinterOptionType], default: LinterOptionType
 ) -> LinterOptionType:
+	'''
+	Read the Python source file from the given file ``filename`` and search for the option by the name of ``name``
+	in the "Magic Comments" in the file.
+
+	The value of ``name`` is parsed according to rules set out by ``type``, and if ``name`` is not found, then the value
+	of ``default`` is returned instead.
+
+	See :py:meth:`get_linter_options` for more information on the "Magic Comment" format.
+
+	Currently the following option types are supported:
+
+	* :py:class:`bool` - The values of ``0``, ``no``, and ``disable`` are ``False`` while ``1``, ``yes``, and ``enable`` are ``True``
+	* :py:class:`int` - Any valid number that can be parsed from ``int(value, 0)``.
+
+	Parameters
+	----------
+	filename : str
+		The name of the Python source file to ingest.
+
+	name : str
+		The name of the option to get.
+
+	type : type[LinterOptionType]
+		The type of the option ``name``.
+
+	default : LinterOptionType
+		The default value to return if ``name`` is not found.
+
+	Returns
+	-------
+	LinterOptionType
+		The value of the option ``name`` if found or ``default`` if it's not
+
+	Raises
+	------
+	TypeError
+		If ``type`` is not a valid option type.
+
+	'''
+
 	if type not in (bool, int):
-		raise TypeError(f'Expected type to be either \'bool\' or \'int\', not \'{type!r}\'')
+		raise TypeError(f'Expected type to be either \'bool\' or \'int\', not {type!r}')
 
 	options = get_linter_options(filename)
 	if name not in options:
