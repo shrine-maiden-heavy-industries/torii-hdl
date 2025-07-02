79e70878cda953047f8d02709835f5bacf4e51ea
Aki Van Ness <aki@lethalbit.net>
2025-07-02 11:58:10 -0700

    torii.lib.vendor.lattice: Stubbed out the lattice ECP5 library

---
+++ torii/lib/vendor/lattice/ecp5.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: BSD-2-Clause
+
+'''
+This module provides wrapper modules for lattice ECP5 primitive blocks.
+
+'''
+
+from ....hdl.ast import Signal
+from ....hdl.dsl import Module
+from ....hdl.ir  import Elaboratable, Instance
