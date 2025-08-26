c08bfc51e12f3886bec4d680261156de25eea883
Aki Van Ness <aki@lethalbit.net>
2025-08-25 19:02:51 -0700

    torii.platform.vendor.lattice.ecp5: Updated `_check_feature` to properly use `PinFeature`

--- torii/platform/vendor/lattice/ecp5.py
+++ torii/platform/vendor/lattice/ecp5.py
@@ -4,7 +4,7 @@ from abc             import abstractmethod
 from collections.abc import Iterable
 from typing          import Literal
 
-from ....build       import Attrs, Clock, Subsignal, TemplatedPlatform
+from ....build       import Attrs, Clock, Subsignal, PinFeature, TemplatedPlatform
 from ....hdl         import ClockDomain, ClockSignal, Const, Instance, Module, Record, Signal
 from ....lib.io      import Pin
 
@@ -635,7 +635,7 @@ class ECP5Platform(TemplatedPlatform):
 		self, pin: Pin, port: Record, attrs: Attrs, invert: bool, names: Iterable[str]
 	) -> Module:
 		self._check_feature(
-			'single-ended input', pin, attrs, valid_xdrs = (0, 1, 2, 4, 7), valid_attrs = True
+			PinFeature.SE_INPUT, pin, attrs, valid_xdrs = (0, 1, 2, 4, 7), valid_attrs = True
 		)
 
 		m = Module()
@@ -652,7 +652,7 @@ class ECP5Platform(TemplatedPlatform):
 		self, pin: Pin, port: Record, attrs: Attrs, invert: bool, names: Iterable[str]
 	) -> Module:
 		self._check_feature(
-			'single-ended output', pin, attrs, valid_xdrs = (0, 1, 2, 4, 7), valid_attrs = True
+			PinFeature.SE_OUTPUT, pin, attrs, valid_xdrs = (0, 1, 2, 4, 7), valid_attrs = True
 		)
 
 		m = Module()
@@ -669,7 +669,7 @@ class ECP5Platform(TemplatedPlatform):
 		self, pin: Pin, port: Record, attrs: Attrs, invert: bool, names: Iterable[str]
 	) -> Module:
 		self._check_feature(
-			'single-ended tristate', pin, attrs, valid_xdrs = (0, 1, 2, 4, 7), valid_attrs = True
+			PinFeature.SE_TRISTATE, pin, attrs, valid_xdrs = (0, 1, 2, 4, 7), valid_attrs = True
 		)
 
 		m = Module()
@@ -687,7 +687,7 @@ class ECP5Platform(TemplatedPlatform):
 		self, pin: Pin, port: Record, attrs: Attrs, invert: bool, names: Iterable[str]
 	) -> Module:
 		self._check_feature(
-			'single-ended input/output', pin, attrs, valid_xdrs = (0, 1, 2, 4, 7), valid_attrs = True
+			PinFeature.SE_INOUT, pin, attrs, valid_xdrs = (0, 1, 2, 4, 7), valid_attrs = True
 		)
 
 		m = Module()
@@ -706,7 +706,7 @@ class ECP5Platform(TemplatedPlatform):
 		self, pin: Pin, port: Record, attrs: Attrs, invert: bool, names: tuple[Iterable[str], Iterable[str]]
 	) -> Module:
 		self._check_feature(
-			'differential input', pin, attrs, valid_xdrs = (0, 1, 2, 4, 7), valid_attrs = True
+			PinFeature.DIFF_INPUT, pin, attrs, valid_xdrs = (0, 1, 2, 4, 7), valid_attrs = True
 		)
 
 		m = Module()
@@ -723,7 +723,7 @@ class ECP5Platform(TemplatedPlatform):
 		self, pin: Pin, port: Record, attrs: Attrs, invert: bool, names: tuple[Iterable[str], Iterable[str]]
 	) -> Module:
 		self._check_feature(
-			'differential output', pin, attrs, valid_xdrs = (0, 1, 2, 4, 7), valid_attrs = True
+			PinFeature.DIFF_OUTPUT, pin, attrs, valid_xdrs = (0, 1, 2, 4, 7), valid_attrs = True
 		)
 
 		m = Module()
@@ -740,7 +740,7 @@ class ECP5Platform(TemplatedPlatform):
 		self, pin: Pin, port: Record, attrs: Attrs, invert: bool, names: tuple[Iterable[str], Iterable[str]]
 	) -> Module:
 		self._check_feature(
-			'differential tristate', pin, attrs, valid_xdrs = (0, 1, 2, 4, 7), valid_attrs = True
+			PinFeature.DIFF_TRISTATE, pin, attrs, valid_xdrs = (0, 1, 2, 4, 7), valid_attrs = True
 		)
 
 		m = Module()
@@ -758,7 +758,7 @@ class ECP5Platform(TemplatedPlatform):
 		self, pin: Pin, port: Record, attrs: Attrs, invert: bool, names: tuple[Iterable[str], Iterable[str]]
 	) -> Module:
 		self._check_feature(
-			'differential input/output', pin, attrs, valid_xdrs = (0, 1, 2, 4, 7), valid_attrs = True
+			PinFeature.DIFF_INOUT, pin, attrs, valid_xdrs = (0, 1, 2, 4, 7), valid_attrs = True
 		)
 
 		m = Module()
