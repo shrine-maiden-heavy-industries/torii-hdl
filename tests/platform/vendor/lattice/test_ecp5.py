a5308e9159ceff89124243c12e1da7c2734bf609
dragonmux <git@dragonmux.network>
2025-08-28 05:42:15 +0100

    tests.platform.vendor.lattice.ecp5: Expanded the extref_good test to check the resulting RTLIL and LPF are correct

--- tests/platform/vendor/lattice/test_ecp5.py
+++ tests/platform/vendor/lattice/test_ecp5.py
@@ -3,7 +3,7 @@
 from unittest                           import TestCase
 
 from torii.hdl                          import Elaboratable, Module
-from torii.build.res                    import Resource, Subsignal, DiffPairs, Pins
+from torii.build.res                    import Resource, Subsignal, DiffPairs, Pins, Attrs
 from torii.platform.vendor.lattice.ecp5 import ECP5Platform
 
 class TestPlatform(ECP5Platform):
@@ -24,11 +24,59 @@ class TestElaboratable(Elaboratable):
 class ECP5PlatformTestCase(TestCase):
 
 	def test_inst_extref_good(self):
-		resource = Resource('extref', 0, DiffPairs('Y11', 'Y12', dir = 'i'))
+		resource = Resource(
+			'extref', 0, DiffPairs('Y11', 'Y12', dir = 'i'),
+			Attrs(RTERM = True, DCBIAS = True))
 		platform = TestPlatform()
 		platform.add_resources((resource, ))
 
-		platform.prepare(TestElaboratable())
+		plan = platform.prepare(TestElaboratable())
+
+		# Grab the RTLIL generated
+		rtlil = plan.files['top.il']
+		assert isinstance(rtlil, str)
+		# Remove the auto-gen line for Torii
+		rtlil_lines = rtlil.splitlines()[1:]
+		# If the line begins with `attribute` (ignoring whitespace), drop it
+		rtlil_lines = list(
+			filter(lambda line: not line.strip().startswith('attribute'), rtlil_lines)
+		)
+		# Recombobulate it and check the result is correct
+		rtlil = '\n'.join(rtlil_lines)
+		self.assertEqual(rtlil, r'''module \top.pin_extref_0
+  wire width 1 input 0 \extref_0__p
+  wire width 1 input 1 \extref_0__n
+  wire width 1 \extref_0__i
+  cell \EXTREFB \extref_0_0
+    parameter \REFCK_DCBIAS_EN 1'1
+    parameter \REFCK_RTERM 1'1
+    parameter \REFCK_PWDNB 1'1
+    connect \REFCLKP \extref_0__p
+    connect \REFCLKN \extref_0__n
+    connect \REFCLKO { \extref_0__i }
+  end
+end
+module \top
+  wire width 1 input 0 \extref_0__p
+  wire width 1 input 1 \extref_0__n
+  cell \top.pin_extref_0 \pin_extref_0
+    connect \extref_0__p \extref_0__p
+    connect \extref_0__n \extref_0__n
+  end
+end''') # noqa: E101
+
+		# Grab the LPF generated
+		lpf = plan.files['top.lpf']
+		assert isinstance(lpf, str)
+		# Remove the auto-gen line for Torii
+		lpf_lines = lpf.splitlines()[1:]
+		# Recombobulate it and check the result is correct
+		lpf = '\n'.join(lpf_lines)
+		self.assertEqual(lpf, r'''BLOCK ASYNCPATHS;
+BLOCK RESETPATHS;
+LOCATE COMP "extref_0__p" SITE "Y11";
+LOCATE COMP "extref_0__n" SITE "Y12";
+# (add_preferences placeholder)''')
 
 	def test_inst_extref_as_pins(self):
 		resource = Resource(
