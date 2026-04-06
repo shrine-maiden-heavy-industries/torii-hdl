62c0e4320a8881a728c15cff9a178fb192809e90
Aki Van Ness <aki@lethalbit.net>
2026-04-06 09:47:58 -0700

    torii.platform.resources.interface.network: Add diagnostics

--- torii/platform/resources/interface/network.py
+++ torii/platform/resources/interface/network.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
-from ....build.dsl import Attrs, Pins, Resource, ResourceConn, SubsigArgT, Subsignal
+from ....build.dsl   import Attrs, Pins, Resource, ResourceConn, SubsigArgT, Subsignal
+from ....diagnostics import ResourceError
+from ....util.tracer import get_src_loc
 
 __all__ = (
 	'CANResource',
@@ -17,14 +19,16 @@ def CANResource(
 	'''
 
 	ios: list[SubsigArgT] = [
-		Subsignal('rx', Pins(rx, dir = 'i', conn = conn)),
-		Subsignal('tx', Pins(tx, dir = 'o', conn = conn)),
+		Subsignal('rx', Pins(rx, dir = 'i', conn = conn, src_loc_at = 1), src_loc_at = 1),
+		Subsignal('tx', Pins(tx, dir = 'o', conn = conn, src_loc_at = 1), src_loc_at = 1),
 	]
 
 	if attrs is not None:
 		ios.append(attrs)
 
-	return Resource.family(name_or_number, number, default_name = 'can', ios = ios)
+	return Resource.family(
+		name_or_number, number, default_name = 'can', ios = ios, src_loc_at = 1
+	)
 
 def EthernetResource(
 	name_or_number: str | int, number: int | None = None, *,
@@ -41,54 +45,89 @@ def EthernetResource(
 	'''
 
 	if len(rxd.split()) not in (4, 8):
-		raise ValueError(
-			f'{len(rxd.split())} names are specified ({rxd}) but one of (4, 8) was expected'
+		raise ResourceError(
+			message = f'{len(rxd.split())} names are specified ({rxd}) but one of (4, 8) was expected',
+			src_loc = get_src_loc()
 		)
 
 	if len(txd.split()) not in (4, 8):
-		raise ValueError(
-			f'{len(txd.split())} names are specified ({txd}) but one of (4, 8) was expected'
+		raise ResourceError(
+			message = f'{len(txd.split())} names are specified ({txd}) but one of (4, 8) was expected',
+			src_loc = get_src_loc()
 		)
 
 	ios: list[SubsigArgT] = [
-		Subsignal('rx_clk', Pins(rxck, dir = 'i', conn = conn, assert_width = 1)),
-		Subsignal('rx_dat', Pins(rxd, dir = 'i', conn = conn)),
-		Subsignal('tx_clk', Pins(txck, dir = 'i', conn = conn, assert_width = 1)),
-		Subsignal('tx_dat', Pins(txd, dir = 'o', conn = conn)),
+		Subsignal('rx_clk', Pins(rxck, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1),
+		Subsignal('rx_dat', Pins(rxd, dir = 'i', conn = conn, src_loc_at = 1), src_loc_at = 1),
+		Subsignal('tx_clk', Pins(txck, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1),
+		Subsignal('tx_dat', Pins(txd, dir = 'o', conn = conn, src_loc_at = 1), src_loc_at = 1),
 	]
 
 	if rx_dv is not None and rx_err is not None:
 		if rx_ctl is not None:
-			raise ValueError('rx_ctl must be None!')
-		ios.append(Subsignal('rx_dv', Pins(rx_dv, dir = 'i', conn = conn, assert_width = 1)))
-		ios.append(Subsignal('rx_err', Pins(rx_err, dir = 'i', conn = conn, assert_width = 1)))
+			raise ResourceError(
+				message = 'rx_ctl must be None!', src_loc = get_src_loc()
+			)
+		ios.append(Subsignal(
+			'rx_dv', Pins(rx_dv, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		))
+		ios.append(Subsignal(
+			'rx_err', Pins(rx_err, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		))
 	elif rx_ctl is not None:
-		ios.append(Subsignal('rx_ctl', Pins(rx_ctl, dir = 'i', conn = conn, assert_width = 1)))
+		ios.append(Subsignal(
+			'rx_ctl', Pins(rx_ctl, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		))
 	else:
-		raise AssertionError('Must specify either MII RXDV + RXER pins or RGMII RXCTL')
+		raise ResourceError(
+			message = 'Must specify either MII RXDV + RXER pins or RGMII RXCTL',
+			src_loc = get_src_loc()
+		)
 
 	if tx_en is not None and tx_err is not None:
 		if tx_ctl is not None:
-			raise ValueError('tx_ctl must be None')
-		ios.append(Subsignal('tx_en', Pins(tx_en, dir = 'o', conn = conn, assert_width = 1)))
-		ios.append(Subsignal('tx_err', Pins(tx_err, dir = 'o', conn = conn, assert_width = 1)))
+			raise ResourceError(
+				message = 'tx_ctl must be None', src_loc = get_src_loc()
+			)
+		ios.append(Subsignal(
+			'tx_en', Pins(tx_en, dir = 'o', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		))
+		ios.append(Subsignal(
+			'tx_err', Pins(tx_err, dir = 'o', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		))
 		if col is not None:
-			ios.append(Subsignal('col', Pins(col, dir = 'i', conn = conn, assert_width = 1)))
+			ios.append(Subsignal(
+				'col', Pins(col, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+			))
 		if crs is not None:
-			ios.append(Subsignal('crs', Pins(crs, dir = 'i', conn = conn, assert_width = 1)))
+			ios.append(Subsignal(
+				'crs', Pins(crs, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+			))
 	elif tx_ctl is not None:
 		if col is not None and crs is not None:
-			raise ValueError('col and crs must not be specified if tx_ctl is')
-		ios.append(Subsignal('tx_ctl', Pins(tx_ctl, dir = 'o', conn = conn, assert_width = 1)))
+			raise ResourceError(
+				message = 'col and crs must not be specified if tx_ctl is',
+				src_loc = get_src_loc()
+			)
+		ios.append(Subsignal(
+			'tx_ctl', Pins(tx_ctl, dir = 'o', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		))
 	else:
-		raise AssertionError('Must specify either MII TXDV + TXER pins or RGMII TXCTL')
+		raise ResourceError(
+			message = 'Must specify either MII TXDV + TXER pins or RGMII TXCTL',
+			src_loc = get_src_loc()
+		)
 
 	assert (rx_dv is not None and rx_err is not None) == (tx_en is not None and tx_err is not None)
 	assert (rx_ctl is not None) == (tx_ctl is not None)
 
 	if mdc is not None and mdio is not None and mdio_attrs is not None:
-		ios.append(Subsignal('mdc', Pins(mdc, dir = 'o', conn = conn, assert_width = 1), mdio_attrs))
-		ios.append(Subsignal('mdio', Pins(mdio, dir = 'io', conn = conn, assert_width = 1), mdio_attrs))
+		ios.append(Subsignal(
+			'mdc', Pins(mdc, dir = 'o', conn = conn, assert_width = 1, src_loc_at = 1), mdio_attrs, src_loc_at = 1
+		))
+		ios.append(Subsignal(
+			'mdio', Pins(mdio, dir = 'io', conn = conn, assert_width = 1, src_loc_at = 1), mdio_attrs, src_loc_at = 1
+		))
 	if attrs is not None:
 		ios.append(attrs)
-	return Resource.family(name_or_number, number, default_name = 'eth', ios = ios)
+	return Resource.family(name_or_number, number, default_name = 'eth', ios = ios, src_loc_at = 1)
