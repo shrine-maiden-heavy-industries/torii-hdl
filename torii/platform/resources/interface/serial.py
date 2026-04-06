10c996da106531dc216417701497856115c3a61b
Aki Van Ness <aki@lethalbit.net>
2026-04-06 09:48:17 -0700

    torii.platform.resources.interface.serial: Add diagnostics

--- torii/platform/resources/interface/serial.py
+++ torii/platform/resources/interface/serial.py
@@ -1,8 +1,10 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
-from typing        import Literal
+from typing          import Literal
 
-from ....build.dsl import Attrs, Pins, PinsN, Resource, ResourceConn, SubsigArgT, Subsignal
+from ....build.dsl   import Attrs, Pins, PinsN, Resource, ResourceConn, SubsigArgT, Subsignal
+from ....diagnostics import ResourceError
+from ....util.tracer import get_src_loc
 
 __all__ = (
 	'I2CResource',
@@ -24,13 +26,17 @@ def I2CResource(
 
 	io: list[SubsigArgT] = []
 
-	io.append(Subsignal('scl', Pins(scl, dir = 'io', conn = conn, assert_width = 1)))
-	io.append(Subsignal('sda', Pins(sda, dir = 'io', conn = conn, assert_width = 1)))
+	io.append(Subsignal(
+		'scl', Pins(scl, dir = 'io', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+	))
+	io.append(Subsignal(
+		'sda', Pins(sda, dir = 'io', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+	))
 
 	if attrs is not None:
 		io.append(attrs)
 
-	return Resource.family(name_or_number, number, default_name = 'i2c', ios = io)
+	return Resource.family(name_or_number, number, default_name = 'i2c', ios = io, src_loc_at = 1)
 
 def IrDAResource(
 	number: int, *,
@@ -44,23 +50,34 @@ def IrDAResource(
 	# Exactly one of en (active-high enable) or sd (shutdown, active-low enable) should
 	# be specified, and it is mapped to a logic level en subsignal.
 	if not (en is not None) ^ (sd is not None):
-		raise ValueError('Only en or sd may be specified, not both!')
+		raise ResourceError(
+			message = 'Only en or sd may be specified, not both!',
+			src_loc = get_src_loc()
+		)
 
 	io: list[SubsigArgT] = []
 
-	io.append(Subsignal('rx', Pins(rx, dir = 'i', conn = conn, assert_width = 1)))
-	io.append(Subsignal('tx', Pins(tx, dir = 'o', conn = conn, assert_width = 1)))
+	io.append(Subsignal(
+		'rx', Pins(rx, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+	))
+	io.append(Subsignal(
+		'tx', Pins(tx, dir = 'o', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+	))
 
 	if en is not None:
-		io.append(Subsignal('en', Pins(en, dir = 'o', conn = conn, assert_width = 1)))
+		io.append(Subsignal(
+			'en', Pins(en, dir = 'o', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		))
 
 	if sd is not None:
-		io.append(Subsignal('en', PinsN(sd, dir = 'o', conn = conn, assert_width = 1)))
+		io.append(Subsignal(
+			'en', PinsN(sd, dir = 'o', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		))
 
 	if attrs is not None:
 		io.append(attrs)
 
-	return Resource('irda', number, *io)
+	return Resource('irda', number, *io, src_loc_at = 1)
 
 def JTAGResource(
 	name_or_number: str | int, number: int | None = None, *,
@@ -72,14 +89,22 @@ def JTAGResource(
 	'''
 
 	ios: list[SubsigArgT] = [
-		Subsignal('tck', Pins(tck, dir = 'i', conn = conn, assert_width = 1)),
-		Subsignal('tms', Pins(tms, dir = 'i', conn = conn, assert_width = 1)),
-		Subsignal('tdi', Pins(tdi, dir = 'i', conn = conn, assert_width = 1)),
-		Subsignal('tdo', Pins(tdo, dir = 'oe', conn = conn, assert_width = 1)),
+		Subsignal(
+			'tck', Pins(tck, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		),
+		Subsignal(
+			'tms', Pins(tms, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		),
+		Subsignal(
+			'tdi', Pins(tdi, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		),
+		Subsignal(
+			'tdo', Pins(tdo, dir = 'oe', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		),
 	]
 	if attrs is not None:
 		ios.append(attrs)
-	return Resource.family(name_or_number, number, default_name = 'jtag', ios = ios)
+	return Resource.family(name_or_number, number, default_name = 'jtag', ios = ios, src_loc_at = 1)
 
 def PS2Resource(
 	name_or_number: str | int, number: int | None = None, *,
@@ -92,13 +117,17 @@ def PS2Resource(
 
 	ios: list[SubsigArgT] = []
 
-	ios.append(Subsignal('clk', Pins(clk, dir = 'i', conn = conn, assert_width = 1)))
-	ios.append(Subsignal('dat', Pins(dat, dir = 'io', conn = conn, assert_width = 1)))
+	ios.append(Subsignal(
+		'clk', Pins(clk, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+	))
+	ios.append(Subsignal(
+		'dat', Pins(dat, dir = 'io', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+	))
 
 	if attrs is not None:
 		ios.append(attrs)
 
-	return Resource.family(name_or_number, number, default_name = 'ps2', ios = ios)
+	return Resource.family(name_or_number, number, default_name = 'ps2', ios = ios, src_loc_at = 1)
 
 def SPIResource(
 	name_or_number: str | int, number: int | None = None, *,
@@ -111,56 +140,98 @@ def SPIResource(
 	'''
 
 	if role not in ('controller', 'peripheral', 'generic'):
-		raise ValueError(f'role must be one of \'controller\', \'peripheral\', or \'generic\' not {role!r}')
+		raise ResourceError(
+			message = f'role must be one of \'controller\', \'peripheral\', or \'generic\' not {role!r}',
+			src_loc = get_src_loc()
+		)
 
 	# support unidirectional SPI
 	if copi is None and cipo is None:
-		raise ValueError('Either COPI or CIPO or both must be set, not none')
+		raise ResourceError(
+			message = 'Either COPI or CIPO or both must be set, not none',
+			src_loc = get_src_loc()
+		)
 
 	io: list[SubsigArgT] = []
 
 	if role == 'controller':
-		io.append(Subsignal('cs', PinsN(cs_n, dir = 'o', conn = conn)))
-		io.append(Subsignal('clk', Pins(clk, dir = 'o', conn = conn, assert_width = 1)))
+		io.append(Subsignal(
+			'cs', PinsN(cs_n, dir = 'o', conn = conn, src_loc_at = 1), src_loc_at = 1
+		))
+		io.append(Subsignal(
+			'clk', Pins(clk, dir = 'o', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		))
 		if copi is not None:
-			io.append(Subsignal('copi', Pins(copi, dir = 'o', conn = conn, assert_width = 1)))
+			io.append(Subsignal(
+				'copi', Pins(copi, dir = 'o', conn = conn, assert_width = 1, src_loc_at =1), src_loc_at = 1
+			))
 		if cipo is not None:
-			io.append(Subsignal('cipo', Pins(cipo, dir = 'i', conn = conn, assert_width = 1)))
+			io.append(Subsignal(
+				'cipo', Pins(cipo, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+			))
 	elif role == 'peripheral':
-		io.append(Subsignal('cs', PinsN(cs_n, dir = 'i', conn = conn, assert_width = 1)))
-		io.append(Subsignal('clk', Pins(clk, dir = 'i', conn = conn, assert_width = 1)))
+		io.append(Subsignal(
+			'cs', PinsN(cs_n, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		))
+		io.append(Subsignal(
+			'clk', Pins(clk, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		))
 		if copi is not None:
-			io.append(Subsignal('copi', Pins(copi, dir = 'i', conn = conn, assert_width = 1)))
+			io.append(Subsignal(
+				'copi', Pins(copi, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at =1
+			))
 		if cipo is not None:
-			io.append(Subsignal('cipo', Pins(cipo, dir = 'oe', conn = conn, assert_width = 1)))
+			io.append(Subsignal(
+				'cipo', Pins(cipo, dir = 'oe', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+			))
 	else: # generic
-		io.append(Subsignal('cs', PinsN(cs_n, dir = 'io', conn = conn, assert_width = 1)))
-		io.append(Subsignal('clk', Pins(clk, dir = 'io', conn = conn, assert_width = 1)))
+		io.append(Subsignal(
+			'cs', PinsN(cs_n, dir = 'io', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		))
+		io.append(Subsignal(
+			'clk', Pins(clk, dir = 'io', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		))
 		if copi is not None:
-			io.append(Subsignal('copi', Pins(copi, dir = 'io', conn = conn, assert_width = 1)))
+			io.append(Subsignal(
+				'copi', Pins(copi, dir = 'io', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+			))
 		if cipo is not None:
-			io.append(Subsignal('cipo', Pins(cipo, dir = 'io', conn = conn, assert_width = 1)))
+			io.append(Subsignal(
+				'cipo', Pins(cipo, dir = 'io', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+			))
 
 	if int is not None:
 		if role == 'controller':
-			io.append(Subsignal('int', Pins(int, dir = 'i', conn = conn)))
+			io.append(Subsignal(
+				'int', Pins(int, dir = 'i', conn = conn, src_loc_at = 1), src_loc_at = 1
+			))
 		elif role == 'peripheral':
-			io.append(Subsignal('int', Pins(int, dir = 'oe', conn = conn, assert_width = 1)))
+			io.append(Subsignal(
+				'int', Pins(int, dir = 'oe', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+			))
 		else: # generic
-			io.append(Subsignal('int', Pins(int, dir = 'io', conn = conn, assert_width = 1)))
+			io.append(Subsignal(
+				'int', Pins(int, dir = 'io', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+			))
 
 	if reset is not None:
 		if role == 'controller':
-			io.append(Subsignal('reset', Pins(reset, dir = 'o', conn = conn)))
+			io.append(Subsignal(
+				'reset', Pins(reset, dir = 'o', conn = conn, src_loc_at = 1), src_loc_at = 1
+			))
 		elif role == 'peripheral':
-			io.append(Subsignal('reset', Pins(reset, dir = 'i', conn = conn, assert_width = 1)))
+			io.append(Subsignal(
+				'reset', Pins(reset, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+			))
 		else:  # generic
-			io.append(Subsignal('reset', Pins(reset, dir = 'io', conn = conn, assert_width = 1)))
+			io.append(Subsignal(
+				'reset', Pins(reset, dir = 'io', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+			))
 
 	if attrs is not None:
 		io.append(attrs)
 
-	return Resource.family(name_or_number, number, default_name = 'spi', ios = io)
+	return Resource.family(name_or_number, number, default_name = 'spi', ios = io, src_loc_at = 1)
 
 def UARTResource(
 	name_or_number: str | int, number: int | None = None, *,
@@ -175,7 +246,10 @@ def UARTResource(
 
 	if any(line is not None for line in (rts, cts, dtr, dsr, dcd, ri)):
 		if role not in ('dce', 'dte'):
-			raise ValueError(f'Invalid role, expected \'dce\' or \'dte\', not \'{role}\'')
+			raise ResourceError(
+				message = f'Invalid role, expected \'dce\' or \'dte\', not \'{role}\'',
+				src_loc = get_src_loc()
+			)
 
 	if role == 'dte':
 		dce_to_dte = 'i'
@@ -186,31 +260,47 @@ def UARTResource(
 
 	io: list[SubsigArgT] = []
 
-	io.append(Subsignal('rx', Pins(rx, dir = 'i', conn = conn, assert_width = 1)))
-	io.append(Subsignal('tx', Pins(tx, dir = 'o', conn = conn, assert_width = 1)))
+	io.append(Subsignal(
+		'rx', Pins(rx, dir = 'i', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+	))
+	io.append(Subsignal(
+		'tx', Pins(tx, dir = 'o', conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+	))
 
 	# NOTE(aki): The mess of `# type: ignore` below is because the dte/dce
 	# 　　　　　　　role determination is hard to see through for mypy
 
 	if rts is not None:
-		io.append(Subsignal('rts', Pins(rts, dir = dte_to_dce, conn = conn, assert_width = 1))) # type: ignore
+		io.append(Subsignal(
+			'rts', Pins(rts, dir = dte_to_dce, conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		)) # type: ignore
 
 	if cts is not None:
-		io.append(Subsignal('cts', Pins(cts, dir = dce_to_dte, conn = conn, assert_width = 1))) # type: ignore
+		io.append(Subsignal(
+			'cts', Pins(cts, dir = dce_to_dte, conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		)) # type: ignore
 
 	if dtr is not None:
-		io.append(Subsignal('dtr', Pins(dtr, dir = dte_to_dce, conn = conn, assert_width = 1))) # type: ignore
+		io.append(Subsignal(
+			'dtr', Pins(dtr, dir = dte_to_dce, conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		)) # type: ignore
 
 	if dsr is not None:
-		io.append(Subsignal('dsr', Pins(dsr, dir = dce_to_dte, conn = conn, assert_width = 1))) # type: ignore
+		io.append(Subsignal(
+			'dsr', Pins(dsr, dir = dce_to_dte, conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		)) # type: ignore
 
 	if dcd is not None:
-		io.append(Subsignal('dcd', Pins(dcd, dir = dce_to_dte, conn = conn, assert_width = 1))) # type: ignore
+		io.append(Subsignal(
+			'dcd', Pins(dcd, dir = dce_to_dte, conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		)) # type: ignore
 
 	if ri is not None:
-		io.append(Subsignal('ri', Pins(ri, dir = dce_to_dte, conn = conn, assert_width = 1))) # type: ignore
+		io.append(Subsignal(
+			'ri', Pins(ri, dir = dce_to_dte, conn = conn, assert_width = 1, src_loc_at = 1), src_loc_at = 1
+		)) # type: ignore
 
 	if attrs is not None:
 		io.append(attrs)
 
-	return Resource.family(name_or_number, number, default_name = 'uart', ios = io)
+	return Resource.family(name_or_number, number, default_name = 'uart', ios = io, src_loc_at = 1)
